20ebd7a0ac1ec65a8a5ce865214ae9270c3741d9
00jup <jupark0369@gmail.com>
2025-08-28 17:36:32 +0900

    Chore: workers num modified

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,10 @@
 # Gunicorn 설정
 bind = "127.0.0.1:8080"
-workers = 4
+workers = 1
 worker_class = "sync"
 worker_connections = 1000
 timeout = 30
-keepalive = 2
+keepalive = 1
 max_requests = 1000
 max_requests_jitter = 100
 preload_app = True
